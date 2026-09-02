d7527291f4f4714ae30f3701248ee4ff6f3ccf64
GuidoLaucke <guido.laucke@gmail.com>
2026-09-03 00:03:00 +0300

    chore: update Zoho CRM functions: zst_testwdfile [2026-09-02]

---
+++ zoho-crm/functions/zst_testwdfile/zst_testwdfile.deluge.js
@@ -0,0 +1,16 @@
+string standalone.ZST_testwdFile()
+{
+info zoho.writer.getMergeFields("bappr58ab82e8af8b45dfa9330efcbecd4162","zst_writer_all");
+/*header = Map();
+header.put("Accept","application/vnd.api+json");
+response = invokeurl
+[
+	url :"https://download.zoho.eu/v1/workdrive/download/" + "bappr139b6d9abf304462b3b4b3537ef0b432"
+	type :GET
+	headers:header
+	connection:"wd_all"
+];
+info "response File:  " + response;
+*/
+return "";
+}
