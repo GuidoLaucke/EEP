a1ca877793c82c26359dbf202816b29822ed551b
GuidoLaucke <guido.laucke@gmail.com>
2026-09-03 00:06:27 +0300

    chore: update Zoho CRM functions: zst_abrechnungsvorschlaege_erstellen_naechst [2026-09-02]

---
+++ zoho-crm/functions/zst_abrechnungsvorschlaege_erstellen_naechst/zst_abrechnungsvorschlaege_erstellen_naechst.deluge.js
@@ -0,0 +1,5 @@
+string button.ZST_Abrechnungsvorschlaege_erstellen_naechst()
+{
+info standalone.ZST_Abrechnungsvorschlaege_erstellen("nächster Monat");
+return "Die Vorschläge wurden erfolgreich erstellt";
+}
