5001069144891f3e282b1b1600aa6121d05fcac3
GuidoLaucke <guido.laucke@gmail.com>
2026-09-03 00:04:25 +0300

    chore: update Zoho CRM functions: ZST_lastschrift_payment_Books [2026-09-02]

---
+++ zoho-crm/functions/ZST_lastschrift_payment_Books/ZST_lastschrift_payment_Books.deluge.js
@@ -0,0 +1,40 @@
+string standalone.ZST_lastschrift_payment_Books()
+{
+booksOrgId = 20080259560;
+c_lastschriften = Collection();
+c_lastschriften = {{"K_idr":235883000000743668,"r_id":235883000002661549,"amount":199.92,"reference":"Frank Buchner Systemgastronomie e.K. - #1666DE"},{"K_idr":235883000000735643,"r_id":235883000002661693,"amount":199.92,"reference":"Albert Systemgastronomie 1291 e.K. - #1291DE"},{"K_idr":235883000000714818,"r_id":235883000002662081,"amount":199.92,"reference":"Ehmann Restaurantbetriebe e.K. - #0338DE"},{"K_idr":235883000000742993,"r_id":235883000002662705,"amount":199.92,"reference":"Fast Food Schröder e.K. - #1639DE"},{"K_idr":235883000000713968,"r_id":235883000002663513,"amount":199.92,"reference":"MYN Terki e.K. - #0295DE"},{"K_idr":235883000000716218,"r_id":235883000002664058,"amount":199.92,"reference":"TJ Gastro 2 GmbH - #0398DE"},{"K_idr":235883000000728693,"r_id":235883000002664490,"amount":199.92,"reference":"Fast Food Schröder e.K. - #0976DE"},{"K_idr":235883000000735343,"r_id":235883000002667778,"amount":199.92,"reference":"Erik Huber Systemgastronomie e.K. - #1277DE"},{"K_idr":235883000000721593,"r_id":235883000002667874,"amount":199.92,"reference":"CFS Systemrestaurant GmbH - #0648DE"},{"K_idr":235883000000720593,"r_id":235883000002668507,"amount":199.92,"reference":"Günter Liebhart - #0602DE"},{"K_idr":235883000000714368,"r_id":235883000002668603,"amount":199.92,"reference":"BAS Systemgastronomie GmbH - #0316DE"},{"K_idr":235883000000735468,"r_id":235883000002669645,"amount":199.92,"reference":"Des Plaines e.K. - #1283DE"},{"K_idr":235883000000723843,"r_id":235883000002670149,"amount":199.92,"reference":"Steinbacher Systemgastronomie e.K. - #0752DE"},{"K_idr":235883000000726868,"r_id":235883000002670197,"amount":199.92,"reference":"DE Gastro Laatzen II GmbH - #0889DE"},{"K_idr":235883000000715643,"r_id":235883000002670389,"amount":199.92,"reference":"DOHNIE Restaurants KG - #0375DE"},{"K_idr":235883000000727043,"r_id":235883000002656122,"amount":199.92,"reference":"Bredberg Karlsruhe GmbH & Co. KG - #0897DE"},{"K_idr":235883000000718568,"r_id":235883000002656506,"amount":199.92,"reference":"Heiderich GmbH & Co. KG - #0506DE"},{"K_idr":235883000000734543,"r_id":235883000002657361,"amount":199.92,"reference":"Des Plaines GmbH & Co. KG - #1238DE"},{"K_idr":235883000000736218,"r_id":235883000002658025,"amount":199.92,"reference":"Haas GmbH & Co. KG - #1329DE"},{"K_idr":235883000000720543,"r_id":235883000002658697,"amount":199.92,"reference":"CFS Fastfood GmbH - #0599DE"},{"K_idr":235883000000721168,"r_id":235883000002659169,"amount":199.92,"reference":"Shahid Baig Systemgastronomie e. K. - #0629DE"},{"K_idr":235883000000714943,"r_id":235883000002659313,"amount":199.92,"reference":"Frank Klingler GmbH - #0343DE"},{"K_idr":235883000000738543,"r_id":235883000002659457,"amount":199.92,"reference":"ELCONO Heimsheim GmbH & Co. KG - #1445DE"},{"K_idr":235883000000732743,"r_id":235883000002659841,"amount":199.92,"reference":"Michael Gollor - #1156DE"},{"K_idr":235883000000716868,"r_id":235883000002661417,"amount":199.92,"reference":"PS HamAh GmbH - #0430DE"},{"K_idr":235883000000728593,"r_id":235883000002661513,"amount":199.92,"reference":"Heiderich GmbH & Co. KG - #0972DE"},{"K_idr":235883000000715543,"r_id":235883000002661753,"amount":199.92,"reference":"TJ Gastro 3 GmbH - #0371DE"},{"K_idr":235883000000742568,"r_id":235883000002662477,"amount":199.92,"reference":"Systemgastronomie Christian Eckstein GmbH & Co. KG - #1621DE"},{"K_idr":235883000000738918,"r_id":235883000002663333,"amount":199.92,"reference":"DENSG Michendorf GmbH - #1461DE"},{"K_idr":235883000000726043,"r_id":235883000002664502,"amount":199.92,"reference":"Dennis Schnell Germersheim KG - #0855DE"},{"K_idr":235883000000734393,"r_id":235883000002661985,"amount":199.92,"reference":"JAM Gastro GmbH & Co. KG - #1231DE"},{"K_idr":235883000000714793,"r_id":235883000002667886,"amount":199.92,"reference":"Heiderich Pad KG - #0337DE"},{"K_idr":235883000000714668,"r_id":235883000002668519,"amount":199.92,"reference":"GT. Fischer Familienrestaurant e.K. - #0331DE"},{"K_idr":235883000000729543,"r_id":235883000002669225,"amount":199.92,"reference":"Ehmann Restaurantbetriebe e.K. - #1013DE"},{"K_idr":235883000000718268,"r_id":235883000002669513,"amount":199.92,"reference":"DeSanto Gastro KG - #0493DE"},{"K_idr":235883000000716018,"r_id":235883000002669705,"amount":199.92,"reference":"Haas GmbH & Co. KG - #0390DE"},{"K_idr":235883000000731068,"r_id":235883000002670161,"amount":199.92,"reference":"Systemgastronomie GmbH & Co. KG Buchholz/Aller - #1080DE"},{"K_idr":235883000000736993,"r_id":235883000002670449,"amount":199.92,"reference":"ICE-Wenz Systemgastronomie e.K. - #1369DE"},{"K_idr":235883000000735243,"r_id":235883000002656230,"amount":199.92,"reference":"FB Schnellrestaurant e.K. - #1272DE"},{"K_idr":235883000000730218,"r_id":235883000002656710,"amount":199.92,"reference":"Friedewald Restaurant GmbH - #1041DE"},{"K_idr":235883000001038275,"r_id":235883000002662681,"amount":199.92,"reference":"Systemgastronomie Christian Eckstein GmbH & Co. KG - #1832DE"},{"K_idr":235883000000738093,"r_id":235883000002663345,"amount":199.92,"reference":"LIKO Food GmbH - #1423DE"},{"K_idr":235883000000736493,"r_id":235883000002663633,"amount":199.92,"reference":"Dennis Schnell Bretten KG - #1343DE"},{"K_idr":235883000000726493,"r_id":235883000002664130,"amount":199.92,"reference":"Heck Rauenberg GmbH - #0874DE"},{"K_idr":235883000000719943,"r_id":235883000002664274,"amount":199.92,"reference":"Frank Klingler GmbH - #0573DE"},{"K_idr":235883000000717068,"r_id":235883000002664466,"amount":199.92,"reference":"Frank Buchner Systemgastronomie e.K. - #0439DE"},{"K_idr":235883000000713118,"r_id":235883000002662895,"amount":199.92,"reference":"FB Schnellrestaurant e.K. - #0244DE"},{"K_idr":235883000000713318,"r_id":235883000002662943,"amount":199.92,"reference":"Heck HD Hebelstraße GmbH - #0253DE"},{"K_idr":235883000000726943,"r_id":235883000002667370,"amount":199.92,"reference":"mib Systemgastronomiegesellschaft m.b.H. & Co. Hansastraße KG - #0892DE"},{"K_idr":235883000000720318,"r_id":235883000002669285,"amount":199.92,"reference":"PS Gastro GmbH - #0588DE"},{"K_idr":235883000000734343,"r_id":235883000002661213,"amount":199.92,"reference":"Gerd Vogel Systemgastronomie - #1227DE"},{"K_idr":235883000000735318,"r_id":235883000002668435,"amount":199.92,"reference":"Shahid Baig Systemgastronomie e. K. - #1276DE"}};
+counter = 0;
+for each  ele in c_lastschriften
+{
+	account_id = ele.get("K_idr");
+	invoice_id = ele.get("r_id");
+	amount = ele.get("amount");
+	reference = ele.get("reference");
+	payment_type = "banktransfer";
+	payment_mp = Map();
+	payment_mp.put("payment_mode",payment_type);
+	payment_mp.put("amount",amount);
+	payment_mp.put("reference_number",reference.substring(0,if(len(reference) > 50,50,len(reference))));
+	invoices_made_list = List();
+	inv_mp = Map();
+	inv_mp.put("invoice_id",invoice_id);
+	inv_mp.put("amount_applied",amount);
+	invoices_made_list.add(inv_mp);
+	payment_mp.put("invoices",invoices_made_list);
+	payment_mp.put("invoice_id",invoice_id);
+	payment_mp.put("amount_applied",amount);
+	payment_mp.put("customer_id",account_id);
+	info payment_mp;
+	response = invokeurl
+	[
+		url :"https://www.zohoapis.eu/books/v3/customerpayments?organization_id=" + booksOrgId
+		type :POST
+		parameters:payment_mp.toString()
+		connection:"books_connection"
+	];
+	info response.get("message");
+	counter = counter + 1;
+}
+info "Counter: " + counter;
+return "";
+}
