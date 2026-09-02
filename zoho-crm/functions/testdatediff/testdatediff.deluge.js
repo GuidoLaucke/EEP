adad662d1eb451497134264953a62b5168a027bf
GuidoLaucke <guido.laucke@gmail.com>
2026-09-03 00:04:42 +0300

    chore: update Zoho CRM functions: testdatediff [2026-09-02]

---
+++ zoho-crm/functions/testdatediff/testdatediff.deluge.js
@@ -0,0 +1,10 @@
+string standalone.testDateDiff()
+{
+abr_beginn = '01-Dec-2023';
+abr_ende = '30-Feb-2024';
+//abr_ende = '31-Mar-2024';
+info abr_beginn;
+info abr_ende;
+info abr_beginn.monthsBetween(abr_ende);
+return "";
+}
